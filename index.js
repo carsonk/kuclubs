e59af65585c46f7d1bf75756a0b805a4f1445c49
Carson Killbreath <kill4992@kettering.edu>
2017-04-26 14:09:19 -0400

    Get functional node sqlite example

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import Promise from 'bluebird';
+import db from 'sqlite';
+ 
+const app = express();
+const port = process.env.PORT || 3000;
+ 
+app.get('/post/:id', async (req, res, next) => {
+  try {
+    const [post, categories] = await Promise.all([
+      db.get('SELECT * FROM Post WHERE id = ?', req.params.id),
+      db.all('SELECT * FROM Category')
+    ]);
+    res.render('post', { post, categories });
+  } catch (err) {
+    next(err);
+  }
+});
+ 
+Promise.resolve()
+  // First, try connect to the database 
+  .then(() => db.open('./database.sqlite3', { Promise }))
+  .catch(err => console.error(err.stack))
+  // Finally, launch Node.js app 
+  .finally(() => { 
+    app.listen(port, () => console.log(`Listening on ${port}!`))
+  });
