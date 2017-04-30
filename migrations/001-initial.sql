2d4d89d16b015560c48354f4422b98a2c82ac880
Carson Killbreath <kill4992@kettering.edu>
2017-04-30 15:09:26 -0400

    Fix primary keys

--- migrations/001-initial.sql
+++ migrations/001-initial.sql
@@ -1,35 +1,35 @@
 -- UP
 
 CREATE TABLE persons (
-    id INT PRIMARY KEY, 
+    id INTEGER PRIMARY KEY, 
     email VARCHAR(255) NOT NULL, 
     first_name VARCHAR(255) NOT NULL, 
     last_name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE faculty (
-    id INT PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     person_id INT NOT NULL,
     department VARCHAR(255),
     FOREIGN KEY(person_id) REFERENCES persons(id)
 );
 
 CREATE TABLE students (
-    id INT PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     person_id INT NOT NULL,
     class INT,
     FOREIGN KEY(person_id) REFERENCES persons(id)
 );
 
 CREATE TABLE clubs (
-    id INT PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     description TEXT,
     date_founded DATE
 );
 
 CREATE TABLE events (
-    id INT PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     club_id INT NOT NULL,
     start_time DATETIME NOT NULL,
     end_time DATETIME NOT NULL,
@@ -39,7 +39,7 @@ CREATE TABLE events (
 );
 
 CREATE TABLE announcements (
-    id INT PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     club_id INT NOT NULL,
     time DATETIME,
     title VARCHAR(255),
