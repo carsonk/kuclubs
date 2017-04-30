99bebc2b6de50bed06dcce556761d3c6ca425da5
Carson Killbreath <kill4992@kettering.edu>
2017-04-30 01:09:31 -0400

    Add some pug pages

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 import express from 'express';
 import Promise from 'bluebird';
 import db from 'sqlite';
+import path from 'path';
  
 const app = express();
 const port = process.env.PORT || 3000;
- 
-app.get('/post/:id', async (req, res, next) => {
+
+app.set('view engine', 'pug');
+
+const publicPath = path.join(__dirname, 'public');
+const bootstrapDistPath = path.join(__dirname, 'node_modules', 'bootstrap', 'dist');
+
+app.use('/static', express.static(publicPath));
+app.use('/static/bootstrap', express.static(bootstrapDistPath));
+
+app.get('/', async (req, res, next) => {
   try {
-    const [post, categories] = await Promise.all([
-      db.get('SELECT * FROM Post WHERE id = ?', req.params.id),
-      db.all('SELECT * FROM Category')
-    ]);
-    res.render('post', { post, categories });
+    const clubs = await db.all('SELECT * FROM clubs');
+    console.log(clubs);
+    res.render('index', { title: 'Home', clubs: clubs });
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/create', async (req, res, next) => {
+  try {
+    const context = { title: 'Register Club' };
+    res.render('create', context);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/club', async (req, res, next) =>{
+  try {
+    res.render('club');
   } catch (err) {
     next(err);
   }
