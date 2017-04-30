0a0154841728a8128e1d1c62c8f3e45a87b031a6
Carson Killbreath <kill4992@kettering.edu>
2017-04-30 01:38:02 -0400

    Add ability to register clubs

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import express from 'express';
 import Promise from 'bluebird';
 import db from 'sqlite';
 import path from 'path';
- 
+import bodyParser from 'body-parser';
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -13,6 +14,11 @@ const bootstrapDistPath = path.join(__dirname, 'node_modules', 'bootstrap', 'dis
 
 app.use('/static', express.static(publicPath));
 app.use('/static/bootstrap', express.static(bootstrapDistPath));
+
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
 
 app.get('/', async (req, res, next) => {
   try {
@@ -31,6 +37,17 @@ app.get('/create', async (req, res, next) => {
   } catch (err) {
     next(err);
   }
+});
+
+app.post('/create', async (req, res, next) => {
+  let name = req.body.name;
+  let description = req.body.description;
+  let dateFounded = req.body.dateFounded;
+
+  console.log(`POST request: name=${name} description=${description} dateFounded=${dateFounded}`);
+
+  db.run('INSERT INTO clubs (name, description, date_founded) VALUES (?, ?, ?)', name, description, dateFounded);
+  res.redirect('/');
 });
 
 app.get('/club', async (req, res, next) =>{
