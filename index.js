480b74034b10a096a20e177204dd8ed766624e5f
Carson Killbreath <kill4992@kettering.edu>
2017-04-30 14:52:26 -0400

    Add ability to create person

--- index.js
+++ index.js
@@ -23,14 +23,13 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 app.get('/', async (req, res, next) => {
   try {
     const clubs = await db.all('SELECT * FROM clubs');
-    console.log(clubs);
     res.render('index', { title: 'Home', clubs: clubs });
   } catch (err) {
     next(err);
   }
 });
 
-app.get('/create', async (req, res, next) => {
+app.get('/create', (req, res, next) => {
   try {
     const context = { title: 'Register Club' };
     res.render('create', context);
@@ -50,9 +49,32 @@ app.post('/create', async (req, res, next) => {
   res.redirect('/');
 });
 
-app.get('/club', async (req, res, next) =>{
+app.get('/person/create', (req, res, next) => {
+  const context = { title: 'Register Person' };
+  res.render('create_person', context); 
+});
+
+app.post('/person/create', async (req, res, next) => {
+  db.run('INSERT INTO persons (email, first_name, last_name) VALUES (?, ?, ?)',
+         req.body.email, req.body.first_name, req.body.last_name);
+  // TODO: Insert records into students or faculty, depending on group selection.
+  res.redirect('/');
+});
+
+app.get('/club/:id', async (req, res, next) => {
   try {
-    res.render('club');
+    const [club, announcements, members] = await Promise.all([
+      db.get('SELECT * FROM clubs WHERE id = ?', req.params.id),
+      db.all('SELECT * FROM announcements WHERE club_id = ?', req.params.id),
+      db.all('SELECT p.*, m.* FROM persons p LEFT JOIN membership m ON m.person_id = p.id WHERE m.club_id = ?', req.params.id)
+    ]);
+    const context = {
+      title: 'Viewing ' + clubs.name, // Does pug clean these strings for us?
+      club: club,
+      announcements: announcements,
+      members: members
+    }
+    res.render('club', context);
   } catch (err) {
     next(err);
   }
